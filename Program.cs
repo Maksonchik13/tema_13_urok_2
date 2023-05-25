549ebf9fe880e4224ff5956d737311d95a16419c
Пользователь <@-PC>
2023-05-25 16:12:04 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//1)
+Random random = new Random();
+int rand = 0;
+do
+{
+    rand = random.Next(1, 11);
+    Console.WriteLine(rand);
+}
+while (rand != 7);
+
+
+//2)
+int sum = 0;
+int n = -1;
+do
+{
+    Console.WriteLine("Введите любое число кроме 0");
+    n=int.Parse(Console.ReadLine());
+    sum += n;
+}
+while (n != 0);
+Console.WriteLine($"сумма = {sum}");
